f6e64c2510347dc60a20dab50d0d810aa389972a
muhammadshiraz <muhammad.shiraz@systemsltd.com>
2021-11-09 10:21:37 +0500

    Updates Fixes

--- lib/.env.dart
+++ lib/.env.dart
@@ -1,3 +1,3 @@
 
-// const String googleAPiKey = 'AIzaSyD94-mXhHFjYg6EPNt-_KOWJARY7VgyPis';
-const String googleAPiKey = 'AIzaSyB3Ivj-68o2YYHdlFcHTvqrhurQUPrj5AY';
+ const String googleAPiKey = 'AIzaSyD94-mXhHFjYg6EPNt-_KOWJARY7VgyPis';
+//const String googleAPiKey = 'AIzaSyB3Ivj-68o2YYHdlFcHTvqrhurQUPrj5AY';
